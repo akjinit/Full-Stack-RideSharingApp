8914c2f5cb5198a2a7654ef45853b9e8f9748774
akshat <akshatjha5787@gmail.com>
2025-12-29 01:09:15 +0530

    bug fixed

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const UserDataContext = createContext();
 
@@ -11,7 +11,7 @@ const UserContext = ({ children }) => {
     },
   });
 
-  
+
   return (
     <div>
       <UserDataContext.Provider value={user}>
